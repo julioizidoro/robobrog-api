fbcfab79b326ef726a60eebcd5519de249e4c3ef
julio.silva <julio.silva@BRO-GCT-020.brognoli.com.br>
2020-03-05 09:50:17 -0300

    Inicial

---
+++ src/main/java/br/com/brognoli/RobobrogApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.brognoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobobrogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobobrogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brognoli/RobobrogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brognoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobobrogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
